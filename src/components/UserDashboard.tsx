9a10c327f2b9cd7f0678a1667792538a7f7e8d15
Joki <josedavidamayar@gmail.com>
2025-05-08 18:47:09 -0500

    feat: create the first component for the user

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,7 +1,26 @@
 import { useState } from "react";
+import UserHeader from "./UserHeader";
+import UserGroups from "./UserGroups";
 
 export default function UserDashboard() {
-  const [card, setCard] = useState("");
+  const [card, setCard] = useState("groups");
 
-  return <h1>Dashboard para el usuariooooo</h1>;
+  /**
+   * change the content based on the name
+   * @param cardname - the name of the card
+   */
+  const toggleCard = (cardname: string) => {
+    setCard(cardname);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col bg-zinc-900">
+      <div className="p-3">
+        <UserHeader toggleCard={toggleCard} card={card} />
+      </div>
+      <div className="flex-1 flex items-center justify-center p-3">
+        {card === "groups" && <UserGroups />}
+      </div>
+    </div>
+  );
 }
